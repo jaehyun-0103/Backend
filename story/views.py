5ba007bcb72c25b6ac68a03cb8e489b508761958
yunseo0000 <ddal0524@inu.ac.kr>
2024-07-05 21:20:12 +0900

    Feat: 선택한 위인 정보 불러오기

--- story/views.py
+++ story/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Story
-from .serializers import GreatsSerializer
+from .serializers import GreatsSerializer, GreatDetailSerializer
 
 class GreatsList(APIView):
     def get(self, request):
@@ -22,3 +22,18 @@ class GreatsList(APIView):
 
         serializer = GreatsSerializer(queryset, many=True)
         return Response(serializer.data)
+
+class GreatDetail(APIView):
+    def get(self, request, pk):
+        user_id = request.data.get('user_id')
+
+        if not user_id:
+            return Response({"detail": "User ID not provided."}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            story = Story.objects.get(pk=pk, is_deleted=False)
+        except Story.DoesNotExist:
+            return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = GreatDetailSerializer(story)
+        return Response(serializer.data, status=status.HTTP_200_OK)
