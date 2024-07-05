7cbeb4a96959564192a688d3dc9de24ec6440d86
박근채 <pgc0419@gmail.com>
2024-07-05 14:38:06 +0900

    Feat: 선택한 분야의 위인 목록 불러오기 구현, result의 puzzlCnt와 연동

--- story/views.py
+++ story/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-
 from rest_framework import generics
 from .models import Story
-from .serializers import AllGreatsSerializer
+from .serializers import GreatsSerializer
 
-class AllGreatsList(generics.ListAPIView):
-    queryset = Story.objects.filter(is_deleted=False)
-    serializer_class = AllGreatsSerializer
+class GreatsList(generics.ListAPIView):
+    serializer_class = GreatsSerializer
+
+    def get_queryset(self):
+        queryset = Story.objects.filter(is_deleted=False)
+        nation = self.request.GET.get('nation')
+        field = self.request.GET.get('field')
+
+        if nation:
+            queryset = queryset.filter(nation=nation)
+        if field:
+            queryset = queryset.filter(field=field)
+
+        return queryset
