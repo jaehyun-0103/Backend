e32c296f83887d6b72bb577c8bfebac0a2468609
조재현 <jaehyun_0103@naver.com>
2024-07-09 15:10:06 +0900

    Feat: Incresement access_cnt & Swagger

--- story/urls.py
+++ story/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import GreatsList, GreatDetail
+from .views import GreatsList, GreatDetail, IncrementAccessCount
 
 urlpatterns = [
     path('', GreatsList.as_view(), name='greats_list'),
     path('<int:story_id>/', GreatDetail.as_view(), name='great_detail'),
+    path('<int:story_id>/talk/', IncrementAccessCount.as_view(), name='increment_access_count'),
 ]
