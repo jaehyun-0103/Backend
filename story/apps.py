6ae73bd4a64c8e2218c36632c8aaaaff55cbe908
조재현 <jaehyun_0103@naver.com>
2024-07-11 00:23:07 +0900

    Fix: Solve Apscheduler Problem

--- story/apps.py
+++ story/apps.py
@@ -1,9 +1,12 @@
 from django.apps import AppConfig
+import os
 
 class StoryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'story'
 
-    # def ready(self):
-    #     from . import jobs
-    #     jobs.start()
+    def ready(self):
+        if os.environ.get('RUN_MAIN', None) is not None:
+            print(' RUN_MAIN :', os.environ.get('RUN_MAIN', None))
+            from . import jobs
+            jobs.start()
