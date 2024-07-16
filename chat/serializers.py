51a3af4eda0b74addc42e1ba09752d6e7ff314e6
박근채 <pgc0419@gmail.com>
2024-07-16 16:21:06 +0900

    Refactor: API 뷰 삭제

--- chat/serializers.py
+++
@@ -1,5 +0,0 @@
-# chat/serializers.py
-from rest_framework import serializers
-
-class GPTChatSerializer(serializers.Serializer):
-    gpt_message = serializers.CharField()
--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,7 @@
 #chat/urls.py
 from django.urls import path
-from .views import ChatTemplateView, GPTChatView
+from .views import ChatTemplateView
 
 urlpatterns = [
     path('<int:story_id>/talk/', ChatTemplateView.as_view(), name='chat'),  # HTML 템플릿 뷰
-    path('<int:story_id>/', GPTChatView.as_view(), name='gptchat'),    # DRF API 뷰
 ]
