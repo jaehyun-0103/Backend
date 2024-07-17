4550d5080968bd0d68694cfe1d6f0bf2c197f9ca
박근채 <pgc0419@gmail.com>
2024-07-17 17:48:58 +0900

    Feat: RAG 연동 완료

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,28 +1,31 @@
 #chat/consumers.py
-import json, logging
+import json, logging, requests, base64, bs4, asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from openai import OpenAI
 from django.conf import settings
 from django_redis import get_redis_connection
-from langchain_community.docstore.wikipedia import Wikipedia
-import requests
-from bs4 import BeautifulSoup
+from langchain import hub
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain.chat_models import ChatOpenAI
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 
 logger = logging.getLogger(__name__)
 
+# 파일 핸들러 추가
+file_handler = logging.FileHandler('application.log')
+file_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
 client = OpenAI(api_key=settings.OPENAI_API_KEY)
 redis_conn = get_redis_connection("default")
-
-# langchain을 이용한 Wikipedia 내용 가져오기
-def get_wikipedia_content(url, max_length=10000):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    content = soup.find_all('p')
-    text_content = ' '.join([para.text for para in content])
-    # 최대 길이 제한
-    if len(text_content) > max_length:
-        text_content = text_content[:max_length]
-    return text_content
 
 class ChatConsumer(AsyncWebsocketConsumer):
     # 비동기식으로 Websocket 연결 되었을 때 로직
@@ -39,7 +42,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         # 각 모델의 초기 인사
         initial_message_map = {
-            '1': "반갑소, 소인 이순신 장군이라 하오.",
+            '1': "반갑소, 소인 이순신 장군이라 하오. 아직 대화할 준비가 안되었으니 잠시 기다려 주시면 감사하겠소.",
             # 파인튜닝 진행될 떄 마다 추가
             # '2': "반갑소, 소인 이순신 장군이라 하오.",
             # '3': "반갑소, 소인 이순신 장군이라 하오.",
@@ -48,8 +51,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
             # '6': "반갑소, 소인 이순신 장군이라 하오.",
             # '7': "반갑소, 소인 이순신 장군이라 하오.",
             # '8': "반갑소, 소인 이순신 장군이라 하오.",
-            # '9': "반갑소, 소인 이순신 장군이라 하오.",
-            # '10': "반갑소, 소인 이순신 장군이라 하오.",
         }
 
         # 초기 인사 메시지 설정
@@ -63,6 +64,104 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=json.dumps({
             'message': initial_message
         }))
+
+        # 벡터 스토어 생성 작업 비동기 실행
+        await self.initialize_vectorstore()
+
+    # 벡터 스토어 초기화 함수
+    # 속도 증진을 위해 웹소켓 연결이 되었을 때 벡터스토어 생성까지 해둔다.
+    async def initialize_vectorstore(self):
+        try:
+            # Wikipedia url 가져오기
+            wikipedia_url_map = {
+                '1': 'https://ko.wikipedia.org/wiki/이순신',
+                # 파인튜닝 진행될 떄 마다 추가
+                # '2': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+                # '3': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+                # '4': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+                # '5': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+                # '6': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+                # '7': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+                # '8': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
+            }
+
+            story_id = self.story_id
+
+            # 단계 1: 문서 로드(Load Documents)
+            # Wikipedia 내용을 로드하고, 청크로 나누고, 인덱싱합니다.
+            async def load_documents(story_id):
+                wikipedia_url = wikipedia_url_map[story_id]
+                loader = WebBaseLoader(
+                    web_paths=(wikipedia_url,),
+                    bs_kwargs=dict(
+                        parse_only=bs4.SoupStrainer(
+                            "div",
+                            attrs={"class": ["mw-content-ltr mw-parser-output"], "lang": ["ko"], "dir": ["ltr"]},
+                        )
+                    ),
+                )
+                docs = loader.load()
+                return docs
+
+            # 단계 2: 문서 분할(Split Documents)
+            async def split_documents(docs):
+                text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=1000)
+                splits = text_splitter.split_documents(docs)
+                return splits
+
+            # 단계 3: 임베딩 & 벡터스토어 생성(Create Vectorstore)
+            # 벡터스토어를 생성합니다.
+            # 속도 개선을 위해 병렬로 처리
+            def create_vectorstore_sync(splits):
+                embeddings = FastEmbedEmbeddings()
+                vectorstore = FAISS.from_documents(documents=splits, embedding=embeddings)
+                return vectorstore
+
+            async def create_vectorstore(splits):
+                loop = asyncio.get_event_loop()
+                with ThreadPoolExecutor() as pool:
+                    vectorstore = await loop.run_in_executor(pool, partial(create_vectorstore_sync, splits))
+                return vectorstore
+
+            # 문서 로드 비동기 실행
+            docs_task = asyncio.create_task(load_documents(story_id))
+            docs = await docs_task
+            logger.info('문서 로드가 완료되었습니다.')
+
+            # 문서 분할 비동기 실행
+            splits_task = asyncio.create_task(split_documents(docs))
+            splits = await splits_task
+            logger.info('문서 분할이 완료되었습니다.')
+
+            # 벡터스토어 생성 비동기 실행
+            vectorstore_task = asyncio.create_task(create_vectorstore(splits))
+            self.vectorstore = await vectorstore_task
+            logger.info('벡터스토어 생성이 완료되었습니다.')
+
+            # 각 모델의 대화 준비 완료 문구
+            ready_message_map = {
+                '1': "오래 기다리셨소, 기다리게 해서 미안하오. 이제야 대화할 준비가 다 되었구려. 무엇이 궁금하시오?",
+                # 파인튜닝 진행될 떄 마다 추가
+                # '2': "반갑소, 소인 이순신 장군이라 하오.",
+                # '3': "반갑소, 소인 이순신 장군이라 하오.",
+                # '4': "반갑소, 소인 이순신 장군이라 하오.",
+                # '5': "반갑소, 소인 이순신 장군이라 하오.",
+                # '6': "반갑소, 소인 이순신 장군이라 하오.",
+                # '7': "반갑소, 소인 이순신 장군이라 하오.",
+                # '8': "반갑소, 소인 이순신 장군이라 하오.",
+            }
+
+            # 준비 완료 메시지 설정
+            if self.story_id in ready_message_map:
+                ready_message = ready_message_map[self.story_id]
+
+            # 클라이언트에게 준비 완료 메시지 전송
+            await self.send(text_data=json.dumps({
+                'message': ready_message
+            }))
+
+        except Exception as e:
+            logger.error(f"벡터스토어 초기화 중 오류 발생: {str(e)}")
 
     # 비동기식으로 Websocket 연결 종료할 때 로직
     async def disconnect(self, close_code):
@@ -150,12 +249,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 # '6': "gpt-3.5-turbo-1106:personal6",
                 # '7': "gpt-3.5-turbo-1106:personal7",
                 # '8': "gpt-3.5-turbo-1106:personal8",
-                # '9': "gpt-3.5-turbo-1106:personal9",
-                # '10': "gpt-3.5-turbo-1106:personal10",
             }
             # 파인튜닝 인식을 위한 인퍼런스
             studying_content_map = {
-                '1': "넌 겸손한 이순신 장군님이 되어 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 장군의 말투로 하면 돼."
+                '1': "넌 겸손한 이순신 장군님에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 장군의 말투로 하면 돼. 영어는 쓰면 안되고 한글만 써."
                 # 파인튜닝 진행될 떄 마다 추가
                 # '2': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
                 # '3': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
@@ -164,29 +261,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 # '6': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
                 # '7': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
                 # '8': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
-                # '9': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
-                # '10': "넌 겸손한 이순신에 빙의해서 사용자와 대화를 진행할거야. 말투는 최대한 일관되게 조선시대 말투로 하면 돼.",
-            }
-
-            # Wikipedia url 가져오기
-            wikipedia_url_map = {
-                '1': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # 파인튜닝 진행될 떄 마다 추가
-                # '2': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '3': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '4': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '5': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '6': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '7': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '8': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '9': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
-                # '10': get_wikipedia_content('https://ko.wikipedia.org/wiki/이순신'),
             }
 
             if self.story_id in model_map:
                 model = model_map[self.story_id]
                 studying_content = studying_content_map[self.story_id]
-                wikipedia_content = wikipedia_url_map[self.story_id]
 
                 # 메시지 리스트가 길 경우 자르기
                 while len(messages_history) > 50:
@@ -198,15 +277,50 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 # "role"이 "assistant"일 때의 가장 최근 2개의 "content" 추출
                 assistant_messages_history = [msg["content"] for msg in messages_history if msg["role"] == "assistant"][-2:]
 
+                 # 단계 4: 검색(Search)
+                # 위키피디아에 포함되어 있는 정보를 검색하고 생성합니다.
+                retriever = self.vectorstore.as_retriever(search_kwargs=dict(k=1))
+                retrieved_docs = retriever.get_relevant_documents(user_message)
+                logger.info(f"검색된 문서: {retrieved_docs}")
+
+                # 단계 5: 프롬프트 생성(Create Prompt)
+                # 프롬프트를 생성합니다.
+                prompt = hub.pull("rlm/rag-prompt")
+                logger.info('프롬포트 생성이 완료되었습니다.')
+
+                def format_docs(docs):
+                    # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+                    return "\n\n".join(doc.page_content for doc in docs)
+                logger.info('문서 합병이 완료되었습니다.')
+
+                # 단계 6: LLM 모델 생성 (기존 모델 불러오기)
+                llm = ChatOpenAI(openai_api_key=settings.OPENAI_API_KEY)
+                logger.info('LLM 모델 생성이 완료되었습니다.')
+
+                # 단계 7: 체인 생성(Create Chain)
+                rag_chain = (
+                        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+                        | prompt
+                        | llm
+                        | StrOutputParser()
+                )
+                logger.info('체인 생성이 완료되었습니다.')
+
+               # 단계 8: 비동기로 체인 실행(Run Chain)
+                rag_response = await asyncio.to_thread(rag_chain.invoke, user_message)
+                logger.info('체인 실행이 완료되었습니다.')
+
                 # 메시지 리스트 구성
                 messages = [
                     # 파인튜닝된 정보 인퍼런스
-                    {"role": "system",
-                     "content": f"{studying_content}, 다음은 인물에 대한 정보야: {wikipedia_content} 인물의 말투가 일관되게 답변해줘."},
-                    {"role": "user", "content": user_message},
+                    {"role": "system", "content": f"{studying_content}, 자아를 잃지 말고 빙의한 인물의 성격, 말투를 무슨 일이 있어도 유지해야 해."},
                     # 캐시에 저장된 대화 내용 불러오기
-                    {"role": "system", "content": f"다음은 너와 대화하는 'user'의 최근 대화 내용이야:{user_messages_history} 'user'가 이 내용에 대해 물으면 인물의 말투가 일관되게 답변해줘."},
-                    {"role": "system", "content": f"다음은 너의 최근 대화 내용이야:{assistant_messages_history} 인물의 말투가 일관되게 답변해줘."},
+                    {"role": "system", "content": f"'role': 'user'의 최근 질문 내용이야:{user_messages_history} 'role': 'user'가 자신이 어떤 질문을 했는지 묻는다면 빙의한 인물의 성격과 말투에 일관되게 답변해줘."},
+                    {"role": "system", "content": f"'role': 'assitant'의 최근 답변 내용이야:{assistant_messages_history} 'role': 'user'가 'role': 'assitant'가 어떤 답변을 했는지 묻는다면 빙의한 인물의 성격과 말투에 일관되게 답변해줘."},
+                    # RAG 연동
+                    {"role": "system",
+                     "content": f"{rag_response}, 이 정보들을 기반으로 빙의한 인물의 성격과 말투를 기반으로 답변을 해줘, 학습된 정보가 없어도 빙의한 인물의 성격과 말투에 일관되게 답변해줘."},
+                    {"role": "user", "content": user_message},
                 ]
 
                 response = client.chat.completions.create(
@@ -229,12 +343,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 return gpt_response
 
         except KeyError as ke:
-            print(f"OpenAI API 응답 처리 중 KeyError: {str(ke)}가 발생했습니다.")
+            logger.error(f"OpenAI API 응답 처리 중 KeyError: {str(ke)}가 발생했습니다.")
             gpt_response = "GPT가 예상하지 못한 응답 형식입니다."
 
         except Exception as e:
-            print(f"OpenAI API를 호출하는 중 Error: {str(e)}가 발생했습니다")
+            logger.error(f"OpenAI API를 호출하는 중 Error: {str(e)}가 발생했습니다")
             gpt_response = f"GPT에서 응답 생성 중 오류가 발생했습니다: {str(e)}"
 
         return gpt_response
-
