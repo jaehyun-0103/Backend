65313a9cfed58fd9a25173ad45563ee86f0ef6dc
조재현 <jaehyun_0103@naver.com>
2024-07-24 17:43:12 +0900

    Fix: cors allowed

--- backend/settings.py
+++ backend/settings.py
@@ -100,6 +100,9 @@ TEMPLATES = [
 # CORS 설정 추가
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:8000",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:8000",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
