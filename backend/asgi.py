4231eaaa232a500cf99d14fd73668e2dd6b047af
박근채 <pgc0419@gmail.com>
2024-08-01 18:30:53 +0900

    Fix: asgi 변경

--- backend/asgi.py
+++ backend/asgi.py
@@ -4,7 +4,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 import chat.routing
-from chat.vectorstore_initializer import initialize_vectorstores
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
@@ -21,12 +20,3 @@ application = ProtocolTypeRouter({
             ),
         ),
 })
-
-# Initialize vectorstores
-async def startup():
-    await initialize_vectorstores()
-
-if __name__ == "__main__":
-    import asyncio
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(startup())
